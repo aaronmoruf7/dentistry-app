01a43a442738b6e8590785d69852488b48260b3e
Aaron Moruf <aaronmoruf@college.harvard.edu>
2024-12-26 14:21:15 -0500

    inventory fetch/create added; using the id from the token payload - auth extension of Request

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,6 +2,8 @@ import 'dotenv/config'; //to get variables from .env
 
 import express from 'express';
 import authController from './app/routes/auth/auth.controller';
+import inventoryController from './app/routes/inventory/inventory.controller';
+
 
 const app = express()
 
@@ -10,6 +12,9 @@ app.use(express.json())
 
 //routes
 app.use('/auth', authController);
+app.use('/inventory', inventoryController);
+
+
 
 //set up server
 const PORT = 3000;
