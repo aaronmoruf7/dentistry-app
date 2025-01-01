4cef7ce92a2c97957ab41874b7776ccfe12d19be
Aaron Moruf <aaronmoruf@college.harvard.edu>
2025-01-01 12:56:04 -0400

    navbar and part of inventory components

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config'; //to get variables from .env
 
 import express from 'express';
+import cors from 'cors';
 import authController from './app/routes/auth/auth.controller';
 import inventoryController from './app/routes/inventory/inventory.controller';
 import servicesController from './app/routes/services/services.controller';
@@ -10,15 +11,18 @@ import purchasesController from './app/routes/purchases/purchases.controller';
 
 const app = express()
 
+//backend and frontend are on different servers
+app.use(cors());
+
 //middleware
 app.use(express.json())
 
 //routes
-app.use('/auth', authController);
-app.use('/inventory', inventoryController);
-app.use('/services', servicesController);
-app.use('/invoices', invoicesController);
-app.use('/purchases', purchasesController);
+app.use('api/auth', authController);
+app.use('api/inventory', inventoryController);
+app.use('api/services', servicesController);
+app.use('api/invoices', invoicesController);
+app.use('api/purchases', purchasesController);
 
 
 //set up server
---
+++ frontend/src/components/Table.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+interface Column {
+    header: string;
+    accessor: string;
+}
+
+interface TableProps {
+    columns: Column[];
+    data: any[];
+}
+
+const Table = ({columns, data}: TableProps) => {
+    return(
+        <table className='record-table'>
+            <thead>
+                <tr>
+                    {columns.map((column,index) => (
+                        <th key={index}>{column.header}</th>
+                    ))}
+                </tr>
+            </thead>
+            <tbody>
+                {data.map((row,rowIndex) => (
+                    <tr key={rowIndex}>
+                        {columns.map((column,colIndex) => (
+                            <td key={colIndex}> row[column.accessor]</td>
+                        ))}
+                    </tr>
+                ))}      
+            </tbody>
+        </table>
+    )
+}
+
+export default Table;
