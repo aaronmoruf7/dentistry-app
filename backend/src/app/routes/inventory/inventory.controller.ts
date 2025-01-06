8c958e018ab1f49469e41047e0bd10e48138b4c5
Aaron Moruf <aaronmoruf@college.harvard.edu>
2025-01-06 15:56:02 -0400

    Edit inventory almost complete

--- backend/src/app/routes/inventory/inventory.controller.ts
+++ backend/src/app/routes/inventory/inventory.controller.ts
@@ -1,5 +1,5 @@
 import {Router, Request, Response, NextFunction} from 'express';
-import { getAllInventory, createInventoryItem } from './inventory.service';
+import { getAllInventory, createInventoryItem, deleteInventoryItem, updateInventoryItem } from './inventory.service';
 import auth from '../auth/auth.middleware'
 import HttpException from '../../models/http-exception.model';
 
@@ -24,11 +24,43 @@ router.post('/', auth.required, async (req: Request, res: Response, next: NextFu
     try {
         const userId = req.auth?.id;
         if (!userId) {
-            throw new HttpException (401, 'User ID is required to fetch inventory');
+            throw new HttpException (401, 'User ID is required to add inventory item');
         }
         const {name, category, quantity, price} = req.body
         const inventory_item = await createInventoryItem ({name, category, quantity, price, userId});
         res.json(inventory_item);
+    } catch (error){
+        next(error);
+    }
+});
+
+//delete inventory item
+router.delete('/:id', auth.required, async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const userId = req.auth?.id;
+        if (!userId) {
+            throw new HttpException (401, 'User ID is required to delete inventory item');
+        }
+        const id = parseInt(req.params.id)
+        await deleteInventoryItem (id);
+    } catch (error){
+        next(error);
+    }
+});
+
+//update inventory item
+router.put('/:id', auth.required, async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        // console.log("Request Body:", req.body);
+        const userId = req.auth?.id;
+        if (!userId) {
+            throw new HttpException (401, 'User ID is required to delete inventory item');
+        }
+        const id = parseInt(req.params.id);
+        const {name, category, quantity, price} = req.body;
+       
+        const updatedItem = await updateInventoryItem ({id, name, category, quantity, price, userId});
+        res.status(200).json(updatedItem);
     } catch (error){
         next(error);
     }
--- backend/src/app/routes/inventory/inventory.service.ts
+++ backend/src/app/routes/inventory/inventory.service.ts
@@ -26,3 +26,61 @@ export const createInventoryItem = async (data: {
     });
 };
 
+export const deleteInventoryItem = async (id: number) => {
+    if (!id){
+        throw new HttpException (401, 'Missing field');
+    }
+    return prisma.inventory.delete({
+        where : {
+            id: id,
+        }
+    });
+};
+
+export const updateInventoryItem = async ( updatedData: {
+    id: number; 
+    name: string;
+    category: string;
+    quantity: number;
+    price?: number;
+    userId: number;
+}) => {
+    // if (!updatedData.id) || !updatedData.name || !updatedData.category || !updatedData.quantity || !updatedData.userId){
+    //     throw new HttpException (401, 'Missing field');
+    // }
+
+    if (!updatedData.id){
+        throw new HttpException (401, 'Missing item id');
+    }
+
+    if (!updatedData.name) {
+        throw new HttpException (401, 'Missing name');
+    }
+
+    if (!updatedData.category) {
+        throw new HttpException (401, 'Missing category');
+    }
+
+    if (!updatedData.quantity) {
+        throw new HttpException (401, 'Missing quantity');
+    }
+    
+    if (!updatedData.userId) {
+        throw new HttpException (401, 'Missing userId');
+    }
+
+    return prisma.inventory.update({
+        where : {
+            id: updatedData.id,
+        },
+        data: {
+            name: updatedData.name,
+            category: updatedData.category,
+            quantity: updatedData.quantity,
+            price: updatedData.price
+        }
+    });
+};
+
+
+
